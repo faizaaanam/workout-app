26998d9d771d60bec150fb8a35b5e97962f6fa29
Imaduddin Mujahid <imad@live.in>
2025-05-03 21:10:53 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,16 @@ const urlsToCache = [
   '/workout-app/',
   '/workout-app/exercise-data.json',
   '/workout-app/gym_icon_192.png',
-  'https://cdn.tailwindcss.com'
+  'https://cdn.tailwindcss.com',
+  // Day 1 image URLs
+  'https://cdn.jefit.com/assets/img/exercises/gifs/86.gif',
+  'https://cdn.jefit.com/assets/img/exercises/gifs/21.gif',
+  'https://cdn.jefit.com/assets/img/exercises/gifs/90.gif',
+  'https://cdn.jefit.com/assets/img/exercises/gifs/818.gif',
+  'https://hips.hearstapps.com/hmg-prod/images/workouts/2016/03/dumbbellcurl-1457043876.gif',
+  'https://hips.hearstapps.com/hmg-prod/images/workouts/2016/03/hammercurl-1456956209.gif',
+  'https://cdn.jefit.com/assets/img/exercises/gifs/874.gif'
+
 ];
 
 self.addEventListener('install', event => {
